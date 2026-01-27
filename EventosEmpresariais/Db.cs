f40d49217ec4141ccdeedfac8f98000d63d5301f
Norberto Ferreira <Norberto.Ferreira.29701@formandos.cinel.pt>
2026-01-27 18:45:30 +0000

    Added SQL Server integration and event loading logic to Windows Forms

---
+++ EventosEmpresariais/Db.cs
@@ -0,0 +1,9 @@
+﻿namespace EventosEmpresariais
+{
+    internal static class Db
+    {
+        // String de ligação à Base de Dados (SQL Server)
+        internal static readonly string StringLigacao =
+            @"Data Source=localhost\SQLEXPRESS;Initial Catalog=EventosEmpresariais;Integrated Security=True;TrustServerCertificate=True;";
+    }
+}
--- EventosEmpresariais/Form1.cs
+++ EventosEmpresariais/Form1.cs
@@ -1,3 +1,7 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+
 namespace EventosEmpresariais
 {
     public partial class FrmEventos : Form
@@ -9,8 +13,36 @@ namespace EventosEmpresariais
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            CarregarEventos();
+        }
+
+        private void CarregarEventos()
+        {
+            
+                try
+                {
+                    using SqlConnection ligacao = new SqlConnection(Db.StringLigacao);
+                    using SqlCommand comando = new SqlCommand("spListarEventos", ligacao);
+
+                    comando.CommandType = CommandType.StoredProcedure;
+
+                    DataTable tabela = new DataTable();
+
+                    ligacao.Open();
+                    using SqlDataReader leitor = comando.ExecuteReader();
+                    tabela.Load(leitor);
+
+                    dgvEventos.DataSource = tabela;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao listar eventos: " + ex.Message);
+                }
+            
 
         }
+
+
 
         private void lblDescricao_Click(object sender, EventArgs e)
         {
